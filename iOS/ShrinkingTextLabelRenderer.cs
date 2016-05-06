7b2ab6bff3165713952be51cc5cc9e62a50fa54c
Xamtastic <anthony.harrison@xamtastic.com>
2016-05-06 18:03:00 +0100

    [FEATURE] Created ShrinkingTextLabel with associated iOS Renderer.

---
+++ iOS/ShrinkingTextLabelRenderer.cs
@@ -0,0 +1,28 @@
+﻿using esperancephone.iOS;
+using esperancephone.Views;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ShrinkingTextLabel), typeof(ShrinkingTextLabelRenderer))]
+
+namespace esperancephone.iOS
+{
+    public class ShrinkingTextLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(
+            ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            var label = Control as UILabel;
+            if (label != null)
+            {
+                label.AdjustsFontSizeToFitWidth = true;
+                label.Lines = 1;
+                label.BaselineAdjustment = UIBaselineAdjustment.AlignCenters;
+                label.LineBreakMode = UILineBreakMode.Clip;
+            }
+        }
+    }
+}
