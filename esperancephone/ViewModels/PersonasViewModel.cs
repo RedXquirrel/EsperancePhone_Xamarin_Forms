fe292fdae4b3383ef579a3b57506ee1993ce9bf9
Xamtastic <anthony.harrison@xamtastic.com>
2016-05-14 21:32:13 +0100

    [WIP] Commanding buttons set up on Personas page.

--- esperancephone/ViewModels/PersonasViewModel.cs
+++ esperancephone/ViewModels/PersonasViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Autofac;
 using esperancephone.Interfaces;
 using esperancephone.Ioc;
@@ -18,6 +20,28 @@ namespace esperancephone.ViewModels
     public class PersonasViewModel : StandardViewModel
     {
         private bool _contactSelected;
+        private bool _personaSelected;
+
+        private PersonaListItemViewModel _selectedListItem;
+
+        public ICommand SelectedPersonaListItemCommand => new Command<PersonaListItemViewModel>((item) =>
+        {
+            Debug.WriteLine($"INFORMATION: Selected Persona List Data Item class is {item.Data.GetType().ToString()}");
+
+            using (var scope = AppContainer.Container.BeginLifetimeScope())
+            {
+                SetSelectedItem(item);
+            }
+        });
+
+        private void SetSelectedItem(PersonaListItemViewModel item)
+        {
+            _selectedListItem = item;
+            if (_selectedListItem != null)
+            {
+                _personaSelected = item.Data.GetType() == typeof (PersonaViewModel);
+            }
+        }
 
         private ObservableCollection<PersonaListItemViewModel> _personaListItems;
         public ObservableCollection<PersonaListItemViewModel> PersonaListItems
@@ -83,12 +107,33 @@ namespace esperancephone.ViewModels
 
                 if (_contactSelected)
                 {
+
                     listItems.Add(new PersonaListItemViewModel()
                     {
                         TemplateSelectorType = PersonaListItemType.Communicate,
                         Data = new CommunicateViewModel()
                         {
                             Label = "Send Persona and Call",
+                            CallCommand = new Command(() =>
+                            {
+                                using (var commandScope = AppContainer.Container.BeginLifetimeScope())
+                                {
+                                    if (_personaSelected)
+                                    {
+                                        // ToDo: SEND PERSONA!!!!!
+                                        var dialService = commandScope.Resolve<IDialService>();
+                                        dialService.Dial(telecommunicationService.CurrentSession.PhoneNumber);
+                                    }
+                                    else
+                                    {
+
+                                        var navigationService = commandScope.Resolve<INavigationService>();
+                                        navigationService.CurrentPage.DisplayAlert("No Persona Selected Alert",
+                                            "Please select a Persona to send from the Persona list", "OK");
+                                    }
+                                }
+                            })
+
                         }
                     });
                     listItems.Add(new PersonaListItemViewModel()
@@ -96,12 +141,28 @@ namespace esperancephone.ViewModels
                         TemplateSelectorType = PersonaListItemType.Communicate,
                         Data = new CommunicateViewModel()
                         {
-                            Label = "Send Persona only"
+                            Label = "Send Persona only",
+                            CallCommand = new Command(() =>
+                            {
+                                using (var commandScope = AppContainer.Container.BeginLifetimeScope())
+                                {
+                                    if (_personaSelected)
+                                    {
+                                        // ToDo: Send Persona !!!
+                                        var navigationService = commandScope.Resolve<INavigationService>();
+                                        navigationService.CurrentPage.DisplayAlert("Sent",
+                                            "The selected Persona has been sent", "OK");
+                                    }
+                                    else
+                                    {
+                                        var navigationService = commandScope.Resolve<INavigationService>();
+                                        navigationService.CurrentPage.DisplayAlert("No Persona Selected Alert",
+                                            "Please select a Persona to send from the Persona list", "OK");
+                                    }
+                                }
+                            })
                         }
                     });
-
-                    var dialService = scope.Resolve<IDialService>();
-                    telecommunicationService.SetDialService(dialService);
 
                     listItems.Add(new PersonaListItemViewModel()
                     {
@@ -111,7 +172,11 @@ namespace esperancephone.ViewModels
                             Label = "Call only",
                             CallCommand = new Command(() =>
                             {
-                                dialService.Dial(telecommunicationService.CurrentSession.PhoneNumber);
+                                using (var commandScope = AppContainer.Container.BeginLifetimeScope())
+                                {
+                                    var dialService = scope.Resolve<IDialService>();
+                                    dialService.Dial(telecommunicationService.CurrentSession.PhoneNumber);
+                                }
                             })
                         }
                     });
