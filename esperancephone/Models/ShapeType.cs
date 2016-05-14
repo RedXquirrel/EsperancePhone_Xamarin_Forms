baa96312f3186df2214067909e492f915ba2f244
Xamtastic <anthony.harrison@xamtastic.com>
2016-05-14 13:21:23 +0100

    [WIP] ShapeView Added + iOS Renderer.

---
+++ esperancephone/Models/ShapeType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esperancephone.Models
+{
+    public enum ShapeType
+    {
+        Box,
+        Circle,
+        CircleIndicator
+    }
+}
